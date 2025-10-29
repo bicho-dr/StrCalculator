4f217d8eec9b0d8498703bdf4a6140827bc1a8f7
bachirdridi <    GitHub>
2025-10-29 10:47:26 +0100

    test two Number passed

--- src/StrCalculate.py
+++ src/StrCalculate.py
@@ -1,3 +1,4 @@
-def StrCalculate(nubmers):
-  if nubmers == 1 :
-    return 1
+def StrCalculate(number_one,number_two):
+  if number_one == number_two :
+       return sum(number_one,number_two)
+  return number_one
